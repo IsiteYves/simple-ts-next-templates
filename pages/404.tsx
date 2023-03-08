69f9ed5e6c1be94ec2940a067b7790b82daa4618
ndzhwr <ndizihiweregis06@gmail.com>
2023-03-08 15:01:17 +0200

    404 ui

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import {  useRouter } from 'next/router'
 
  const NotFound = () => {
-    const router = useRouter()
-    
     return (
         <div className="w-fit h-fit mx-auto mt-[40vh]">
             <h1 className="text-4xl font-bold">Page not found</h1>
             <p className="text-gray-600">This page is currently not available <br /></p>
             <code>Error 0001</code> <br /><br />
-            <button className="text-blue-500" onClick={() =>  router.push('/')}>&lt;&nbsp;Back to home</button>
+            <button className="text-green bg-green  bg-opacity-10 rounded-full py-3 px-6" onClick={() =>  history.back()}>Go Back</button>
         </div>
     )
 }
